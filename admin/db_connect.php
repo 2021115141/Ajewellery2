ee97bd725d720abe5ff0a774cd03968c34019e34
2021115141 <116062343+2021115141@users.noreply.github.com>
2023-02-21 23:19:10 +0800

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,8 @@
 <?php 
-
-$conn= new mysqli('localhost','root','','Ajewellery_db')or die("Could not connect to mysql".mysqli_error($con));
+$username = doadmin;
+$password = AVNS_c5AduIX-PkkeiPNMrcx;
+$host = ajewellery-do-user-12863890-0.b.db.ondigitalocean.com;
+$port = 25060;
+$database = defaultdb;
+$sslmode = REQUIRED;
+$conn= new mysqli('$host','$username','$password','$database')or die("Could not connect to mysql".mysqli_error($con));
