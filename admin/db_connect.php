f68f21c0271e22c6507a0c95e108c20a462972ea
2021115141 <116062343+2021115141@users.noreply.github.com>
2023-02-21 23:31:20 +0800

    Update db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -5,4 +5,4 @@ $host = ajewellery-do-user-12863890-0.b.db.ondigitalocean.com;
 $port = 25060;
 $database = defaultdb;
 $sslmode = REQUIRED;
-$conn= new mysqli('$host','$username','$password','$database')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('sql12.freemysqlhosting.net','sql12600014','wqnc4fyTwp','sql12600014')or die("Could not connect to mysql".mysqli_error($con));
